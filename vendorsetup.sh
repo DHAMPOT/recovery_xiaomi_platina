c69a516488ea9302829537da09ae1fcd55d15164
D4rkKnight21 <yusuff8621@gmail.com>
2021-07-18 10:54:56 +0700

    platina: OrangeFox bringup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,5 +16,82 @@
 # limitations under the License.
 #
 
-add_lunch_combo omni_platina-userdebug
-add_lunch_combo omni_platina-eng
+FDEVICE="platina"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export PLATFORM_VERSION="16.1.0"
+	export PLATFORM_SECURITY_PATCH="2099-12-31"
+	export TW_DEFAULT_LANGUAGE="en"
+	#export OF_KEEP_FORCED_ENCRYPTION=1
+	export OF_PATCH_AVB20=1
+	export OF_SCREEN_H=2340
+	export OF_STATUS_H=80
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	#export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1; # if you disable this, then enable the next line
+	export OF_NO_MIUI_PATCH_WARNING=1
+	export OF_USE_GREEN_LED=0
+
+	# use magisk 21.4 for the magisk addon
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
+
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_ZIP_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_REPLACE_BUSYBOX_PS=1
+	export OF_USE_NEW_MAGISKBOOT=1
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+	# use system (ROM) fingerprint where available
+	export OF_USE_SYSTEM_FINGERPRINT=1
+
+	# OTA for custom ROMs
+	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+
+	# -- add settings for R11 --
+	export FOX_R11=1
+	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+	export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+	export FOX_ADVANCED_SECURITY=1
+	# -- end R11 settings --
+
+	# run a process after formatting data to work-around MTP issues
+	export OF_RUN_POST_FORMAT_PROCESS=1
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+
+  	for var in eng user userdebug; do
+  		add_lunch_combo omni_"$FDEVICE"-$var
+  	done
+fi
+#
